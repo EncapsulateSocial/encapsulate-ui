1bbf30cff3afd7752ea4d0885a200d75751ffd57
Alexander Hom <alexanderhom19@gmail.com>
2022-06-25 10:29:45 -0700

    update frontend, create multiple comps and pages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import RegisterPage from './views/Register/RegisterPage/RegisterPage';
+import LoginPage from './views/Login/LoginPage/LoginPage';
+import BlogHomePage from './views/Blog/BlogHomePage/BlogHomePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/blog" element={<BlogHomePage />} />
+    </Routes>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/views/Blog/BlogHomePage/BlogHomePage.js
@@ -0,0 +1,9 @@
+const BlogHomePage = () => {
+    return (
+        <div>
+            <h1>Blog home page</h1>
+        </div>
+    );
+}
+
+export default BlogHomePage;
