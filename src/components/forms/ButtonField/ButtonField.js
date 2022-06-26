d901659a60b5bbb8d640414f654d2a6fbd1ed752
Alexander Hom <alexanderhom19@gmail.com>
2022-06-25 22:36:08 -0700

    update login form

--- src/components/forms/ButtonField/ButtonField.js
+++ src/components/forms/ButtonField/ButtonField.js
@@ -3,12 +3,14 @@ import '../../../sass/components/ButtonField.scss';
 const ButtonField = ({ 
     label,
     type,
-    fill = true
+    fill = true,
+    handler
 }) => {
     return (
         <button 
             className={`ButtonField ${fill ? "ButtonField--filled" : ""}`}
             type={type}
+            onClick={handler}
         > 
             {label}
         </button>
--- src/views/Login/LoginForm/LoginForm.js
+++ src/views/Login/LoginForm/LoginForm.js
@@ -1,32 +1,58 @@
 import '../../../sass/layout/_forms.scss';
 import TextField from '../../../components/forms/TextField/TextField';
 import ButtonField from '../../../components/forms/ButtonField/ButtonField';
-import { Link } from 'react-router-dom';
-
-// Handle login form submission
-const handleLoginFormSubmit = (e) => {
-    // Prevent the page from reloading
-    e.preventDefault();
-
-    // TODO: implement logic for login for submission
-};
+import { Link, useNavigate } from 'react-router-dom';
 
 const LoginForm = () => {
+    // For redirecting to another page
+    let navigate = useNavigate();
+
+    // Handle login form submission
+    const handleLoginFormSubmit = (e) => {
+        // Prevent the page from reloading
+        e.preventDefault();
+
+        // TODO: implement logic for login for submission
+    };
+
+    // Handle create account button click
+    const handleCreateAccountClick = (e) => {
+        e.preventDefault();
+        navigate("/register");
+    }
+
     return (
         <form className="LoginForm" onSubmit={handleLoginFormSubmit}>
             <h2 className="LoginForm__title">Sign Into Account</h2>
-            <div className="LoginForm__input-container">
-                <TextField type="text" label="Email or username" />
-                <TextField type="password" label="Password" />
-            </div>
-            <div className="LoginForm__options-container">
-                <div className="LoginForm__remember-me">
-                    <input type="checkbox" id="remember-me" />
-                    <label htmlFor="remember-me">Remember Me</label>
+            <div className="LoginForm__login-section">
+                <div className="LoginForm__input-container">
+                    <TextField type="text" label="Email or username" />
+                    <TextField type="password" label="Password" />
                 </div>
-                <Link className="LoginForm__forgot-password" to="/forgotPassword">Forgot password?</Link>
+                <div className="LoginForm__options-container">
+                    <div className="LoginForm__remember-me">
+                        <input type="checkbox" id="remember-me" />
+                        <label htmlFor="remember-me">Remember Me</label>
+                    </div>
+                    <Link 
+                        className="LoginForm__forgot-password" 
+                        to="/forgotPassword"
+                    >
+                        Forgot password?
+                    </Link>
+                </div>
+                <ButtonField label="Login" type="text" />
             </div>
-            <ButtonField label="Login" type="text" />
+            <div className="LoginForm__create-account-section">
+                <p className="LoginForm__create-account-description">
+                    Or, if you don't have an account
+                </p>
+                <ButtonField 
+                    label="Create account" 
+                    type="text" fill={false} 
+                    handler={handleCreateAccountClick}
+                />
+            </div>
         </form>
     );
 };
