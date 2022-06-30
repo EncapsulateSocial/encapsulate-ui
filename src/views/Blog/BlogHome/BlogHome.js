3fe73ddf007e279327986ddba8b34899c8bcb44e
Alexander Hom <alexanderhom19@gmail.com>
2022-06-30 16:13:53 -0700

    blog home page JSX and styling

--- src/views/Blog/BlogHome/BlogHome.js
+++ src/views/Blog/BlogHome/BlogHome.js
@@ -1,7 +1,25 @@
+import SearchBar from "../../../components/search/SearchBar";
+import PostListing from "../../../components/blog/postListing/PostListing";
+
 const BlogHome = () => {
     return (
-        <div>
-            <h1>Blog home page</h1>
+        <div className="BlogHome">
+            <div className="BlogHome__about">
+                <h1 className="BlogHome__heading">Development Blog</h1>
+                <p className="BlogHome__heading-caption">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui 
+                    iusto at repellendus laborum alias est a minima cumque facilis 
+                    soluta.
+                </p>
+            </div>
+            <div className="BlogHome__menu">
+                <SearchBar />
+            </div>
+            <div className="BlogHome__posts-container">
+                {[...Array(9)].map(() => (
+                    <PostListing />
+                ))}
+            </div>
         </div>
     );
 }
