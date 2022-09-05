8a3d7ce14fd219207cb9cd4b249adf685aeae502
Alexander Hom <alexanderhom19@gmail.com>
2022-09-04 19:54:31 -0700

    Update ButtonField, TextField

--- src/components/forms/ButtonField/ButtonField.js
+++ src/components/forms/ButtonField/ButtonField.js
@@ -4,11 +4,15 @@ const ButtonField = ({
     label,
     type = "",
     fill = true,
+    fullWidth = false,
     handler = null
 }) => {
     return (
         <button 
-            className={`ButtonField ${fill ? "ButtonField--filled" : ""}`}
+            className={`ButtonField 
+                ${fill ? "ButtonField--filled" : ""}
+                ${fullWidth ? "ButtonField--full-width" : ""}
+            `}
             type={type}
             onClick={handler}
         > 
