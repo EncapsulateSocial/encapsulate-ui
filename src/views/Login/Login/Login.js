e69325e409bed53510c0eb0921d4178d8ff0124f
Alexander Hom <alexanderhom19@gmail.com>
2022-09-20 20:00:39 -0700

    consolidate jsx to single file for login/register

--- src/views/Login/Login/Login.js
+++ src/views/Login/Login/Login.js
@@ -1,14 +1,69 @@
-import React from 'react'; 
-import Header from '../../../components/headers/Header';
-import LoginForm from "../LoginForm/LoginForm";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import TextField from "../../../components/forms/TextField/TextField";
+import ButtonField from "../../../components/forms/ButtonField/ButtonField";
+import Header from "../../../components/headers/Header";
 
 const Login = () => {
-    return (
-        <div className="Login">
-            <Header className="Login__header" simplify={true} />
-            <LoginForm />
+  // For redirecting to another page
+  let navigate = useNavigate();
+
+  // Handle login form submission
+  const handleLoginFormSubmit = (e) => {
+    // Prevent the page from reloading
+    e.preventDefault();
+
+    // TODO: implement logic for login for submission
+  };
+
+  // Handle create account button click
+  const handleCreateAccountClick = (e) => {
+    e.preventDefault();
+    navigate("/register");
+  };
+
+  return (
+    <div className="Login">
+      <Header className="Login__header" simplify={true} />
+
+      <form className="Login__login-form" onSubmit={handleLoginFormSubmit}>
+        <h2 className="Login__login-form-title">Sign Into Account</h2>
+        <div className="Login__login-form-login-section">
+          <div className="Login__login-form-input-container">
+            <TextField type="text" label="Email or username" />
+            <TextField type="password" label="Password" />
+          </div>
+          <div className="Login__login-form-options-container">
+            <Link
+              className="Login__login-form-forgot-password"
+              to="/forgotPassword"
+            >
+              Forgot password?
+            </Link>
+          </div>
+          <ButtonField
+            className="Login__login-form-login-button"
+            label="Login"
+            type="text"
+            fullWidth={true}
+          />
         </div>
-    );
+        <div className="Login__login-form-create-account-section">
+          <p className="Login__login-form-create-account-description">
+            Or, if you don't have an account
+          </p>
+          <ButtonField
+            className="Login__login-form-create-account-button"
+            label="Create account"
+            type="text"
+            fill={false}
+            fullWidth={true}
+            handler={handleCreateAccountClick}
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- src/views/Register/Register/Register.js
+++ src/views/Register/Register/Register.js
@@ -1,14 +1,60 @@
-import React from 'react'; 
-import Header from '../../../components/headers/Header';
-import RegisterForm from "../RegisterForm/RegisterForm";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import TextField from "../../../components/forms/TextField/TextField";
+import ButtonField from "../../../components/forms/ButtonField/ButtonField";
+import Header from "../../../components/headers/Header";
 
 const Register = () => {
-    return (
-        <div className="Register">
-            <Header simplify={true} />
-            <RegisterForm />
+  let navigate = useNavigate();
+
+  const handleRegisterFormSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleSignInClick = (e) => {
+    e.preventDefault();
+    navigate("/login");
+  };
+
+  return (
+    <div className="Register">
+      <Header simplify={true} />
+
+      <form
+        className="Register__register-form"
+        onSubmit={handleRegisterFormSubmit}
+      >
+        <h2 className="Register__register-form-title">Create an Account</h2>
+        <div className="Register__register-form-register-section">
+          <div className="Register__register-form-input-container">
+            <TextField type="text" label="Username" />
+            <TextField type="text" label="Email address" />
+            <TextField type="password" label="Password" />
+            <TextField type="password" label="Confirm password" />
+          </div>
+          <ButtonField
+            className="Register__register-form-sign-up-button"
+            label="Sign up"
+            type="submit"
+            fullWidth={true}
+          />
         </div>
-    );
+        <div className="Register__register-form-sign-in-section">
+          <p className="Register__register-form-sign-in-description">
+            Or, if you already have an account
+          </p>
+          <ButtonField
+            className="Register__register-form-sign-in-button"
+            label="Sign in"
+            type="text"
+            fill={false}
+            fullWidth={true}
+            handler={handleSignInClick}
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
