d4353a545a21beae0e79c03e157c1b7f5843d1c5
Alexander Hom <alexanderhom19@gmail.com>
2022-09-20 21:56:16 -0700

    add tests to login

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -1,14 +1,13 @@
-import React from 'react'; 
-import "../../sass/components/SearchBar.scss";
+import React from "react";
 import { Search } from "@mui/icons-material";
 
 const SearchBar = () => {
-    return (
-        <div className="SearchBar">
-            <input className="SearchBar__input" placeholder="Search" />
-            <Search className="SearchBar__icon" />
-        </div>
-    );
+  return (
+    <div className="SearchBar">
+      <input className="SearchBar__input" placeholder="Search" />
+      <Search className="SearchBar__icon" />
+    </div>
+  );
 };
 
 export default SearchBar;
